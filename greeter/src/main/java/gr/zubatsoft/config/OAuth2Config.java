429b02b294724b22eb230d8126bff6df636b8e60
Kostandin Tahiri <kostahiri@gmail.com>
2017-05-09 16:27:17 +0300

    Similar example :

--- greeter/src/main/java/gr/zubatsoft/config/OAuth2Config.java
+++ greeter/src/main/java/gr/zubatsoft/config/OAuth2Config.java
@@ -10,15 +10,12 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
-
-	private static String REALM = "MY_OAUTH_REALM";
 
 	@Autowired
 	private TokenStore tokenStore;
@@ -40,20 +37,13 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 				.authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
 				.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
 				.scopes("read", "write", "trust")
-				.accessTokenValiditySeconds(120).// Access token is only valid
-													// for 2 minutes.
-				refreshTokenValiditySeconds(600);// Refresh token is only valid
-													// for 10 minutes.
+				.accessTokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(1)).
+				refreshTokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(1));
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
 				.authenticationManager(authenticationManager);
-	}
-
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-		oauthServer.realm(REALM + "/client");
 	}
 }
