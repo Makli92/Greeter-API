80c04e29bf6b5ec0b0d246e05ca55c92ac2edb9b
Kostandin Tahiri <kostahiri@gmail.com>
2017-05-12 16:26:08 +0300

    FlywayDB migration tool included, configuration and test pending.

--- greeter/src/main/resources/db/migration/V0_1__Create_oauth2_clients.sql
+++ greeter/src/main/resources/db/migration/V0_1__Create_oauth2_clients.sql
@@ -1,19 +1,3 @@
-CREATE TABLE IF NOT EXISTS oauth_access_token (
-  token_id VARCHAR(128),
-  token BLOB,
-  authentication_id VARCHAR(128) PRIMARY KEY,
-  user_name VARCHAR(128),
-  client_id VARCHAR(128),
-  authentication BLOB,
-  refresh_token VARCHAR(128)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS oauth_refresh_token (
-  token_id VARCHAR(128),
-  token BLOB,
-  authentication BLOB
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE oauth_client_details (
       client_id VARCHAR(128) PRIMARY KEY,
       resource_ids VARCHAR(128),
